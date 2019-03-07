8b463459afcbc8ab0a2ea41cac399329c6738caf
Rafael <rafa.comp@gmail.com>
2019-03-06 22:37:24 -0300

    eslint-disable-next-line no-console

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,9 @@ import { createStore, compose } from 'redux';
 
 import reducers from './reducers';
 
-const tronMiddleware = process.env.NODE_ENV === 'development' ? console.tron.createEnhancer : () => { };
+const tronMiddleware = process.env.NODE_ENV === 'development'
+  // eslint-disable-next-line no-console
+  ? console.tron.createEnhancer : () => { };
 
 const store = createStore(reducers, compose(tronMiddleware()));
 
