74c9da2eb22e22061e2389749af4509ba3ffb952
Rafael <rafa.comp@gmail.com>
2019-03-07 20:30:55 -0300

    commit do estudo completo

---
+++ src/pages/main/index.js
@@ -0,0 +1,77 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as FavoriteActions } from '../../store/ducks/favorites';
+
+class Main extends Component {
+  static propTypes = {
+    addFavoriteRequest: PropTypes.func.isRequired,
+    favorites: PropTypes.shape({
+      loading: PropTypes.bool,
+      data: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        description: PropTypes.string,
+        url: PropTypes.string,
+      })),
+      error: PropTypes.string,
+    }).isRequired,
+  }
+
+  state = {
+    repositoryInput: '',
+  }
+
+  handleAddRepository = (event) => {
+    const { addFavoriteRequest } = this.props;
+    const { repositoryInput } = this.state;
+    event.preventDefault();
+    addFavoriteRequest(repositoryInput);
+    this.setState({ repositoryInput: '' });
+  }
+
+  render() {
+    const { repositoryInput } = this.state;
+    // eslint-disable-next-line react/prop-types
+    const { favorites } = this.props;
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuario/repositorio"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">Adicionar</button>
+          {favorites.loading && <span>Carregando...</span>}
+          {!!favorites.error && <span style={{ color: '#F00' }}>{favorites.error}</span>}
+        </form>
+
+        <ul>
+          {favorites.data.map(favorite => (
+            <li key={favorite.id}>
+              <p>
+                <strong>{favorite.name}</strong>
+                {' '}
+                (
+                {favorite.description}
+                )
+              </p>
+              <a href={favorite.url}>Acessar</a>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  favorites: state.favorites,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,28 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import reducers from './ducks';
+import sagas from './sagas';
 
-import reducers from './reducers';
+const middlewares = [];
+
+const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+middlewares.push(sagaMiddleware);
 
 const tronMiddleware = process.env.NODE_ENV === 'development'
   // eslint-disable-next-line no-console
   ? console.tron.createEnhancer : () => { };
 
-const store = createStore(reducers, compose(tronMiddleware()));
+const store = createStore(
+  reducers,
+  compose(
+    tronMiddleware(),
+    applyMiddleware(...middlewares),
+  ),
+);
+
+sagaMiddleware.run(sagas);
 
 export default store;
