f6c6fe5cc5c6ce3d71bfcb115fc1089ba0c669b1
Rafael <rafa.comp@gmail.com>
2019-03-06 21:43:47 -0300

    criando action no todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 
-const TodoList = ({ todos }) => (
-  <ul>
-    {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
-  </ul>
+const TodoList = ({ todos, addTodo }) => (
+  <Fragment>
+    <ul>
+      {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+    </ul>
+    <button onClick={() => addTodo('Novo todo')} type="button">Adicionar</button>
+  </Fragment>
 );
-
-const mapStateToProps = state => ({
-  todos: state.todos,
-});
 
 TodoList.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number,
     text: PropTypes.string,
   })).isRequired,
+  addTodo: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps)(TodoList);
+const mapStateToProps = state => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch => ({
+  addTodo: text => dispatch({ type: 'ADD_TODO', payload: { text } }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
